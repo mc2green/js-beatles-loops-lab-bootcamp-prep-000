946d624073adcf4841c32e1048cd5cc408af8c11
Matthew Chen <mc.2green@gmail.com>
2019-07-18 15:17:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // add solution here
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const facts = [
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"];
 
 function theBeatlesPlay(musicians, instruments){
   var counter = 0;
-  var sentence;
+  var sentence = "";
   var band = [];
   for(counter, counter < musicians.length(),counter++ ){
     sentence = musicians[counter] + "plays the" + instruments[counter];
@@ -13,9 +18,20 @@ function theBeatlesPlay(musicians, instruments){
   return band[counter];
 }
 
-function johnLennonFacts{
+function johnLennonFacts(facts){
+  var counter = 0;
+  var exclamation = "";
+  while(counter < facts.length()){
+    exclamation = facts[counter] + "!!!";
+    counter++;
+  }
+  return exclamation
   
 }
+function iLoveBeatles(){
+  
+}
+
 
 
   
