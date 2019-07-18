16f8012e4936ebfa35a78b34c6b85dd7c0a8413d
Matthew Chen <mc.2green@gmail.com>
2019-07-18 15:01:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 var counter = 0;
+var sentence;
 var band = [];
 function theBeatlesPlay(musicians, instruments){
   for(counter, musicians.length(),counter++ ){
+    sentence[counter] = musicians[counter]
     
   }
   
