5d14a051413206f82870399c4e0380b1fdc456b2
Matthew Chen <mc.2green@gmail.com>
2019-07-18 14:59:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // add solution here
-
-function theBeatlesPlay(arrayMusician, arrayInstrument){
-  var newArray = [];
-  var counter = 0
-  for ()
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+var counter = 0;
+var band = [];
+function theBeatlesPlay(musicians, instruments){
+  for(counter, musicians.length(),counter++ ){
+    
+  }
   
 }
