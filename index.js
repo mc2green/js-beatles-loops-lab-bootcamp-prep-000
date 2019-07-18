d83b5d4b975a90a461d34919ddfa58cce75bdc57
Matthew Chen <mc.2green@gmail.com>
2019-07-18 15:07:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 // add solution here
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-var counter = 0;
-var sentence;
-var band = [];
 
 function theBeatlesPlay(musicians, instruments){
-var musicianString;
-var musicianInstrument=[];
-for(var i=0; i<musicians.length; i++){
-musicianString= musicians[i] + ‘ plays ‘ + instruments[i];
-musicianInstrument[i]=musicianString;
+  var counter = 0;
+  var sentence;
+  var band = [];
+  for(counter, counter < musicians.length(),counter++ ){
+    sentence = musicians[counter] + "plays the" + instruments[counter];
+    band[counter] = sentence;
+  }
+  return band[counter];
 }
-return musicianInstrument;
-}
+
 
   
